6137ac056e59c85c5c7f3636cc0a501b1c4d94db
Doug Anson <doug.anson@arm.com>
2018-05-14 22:04:02 -0500

    updates

--- doug/connector-bridge/runService.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -f ./logs/*.log 2> /dev/null
-mkdir -p ./logs
-cd /home/arm/connector-bridge/target
-java -Dconfig_file="../conf/service.properties" -jar connector-bridge-1.0.war 2>&1 1> ../logs/connector-bridge.log
